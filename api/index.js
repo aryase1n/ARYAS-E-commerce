161f8c952834c63ad71a0926ed56cab4dec44be0
aryase1n <124655961+aryase1n@users.noreply.github.com>
2023-08-22 21:48:33 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,12 @@ mongoose
     console.log(err);
   });
 
-app.use(cors());
+app.use(cors(
+  {origin: ["https://aryas-e-commerce.vercel.app"],
+   methods: ["POST", "GET"], 
+   credentials: true
+  }
+));
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
